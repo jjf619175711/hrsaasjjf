6a8b7093e6de7815ecdc2daa94a34ab95e15eaf5
Arthur <619175711@qq.com>
2022-11-08 15:56:24 +0800

    人资项目初始化

--- src/api/user.js
+++ src/api/user.js
@@ -1,24 +1,8 @@
 import request from '@/utils/request'
 
-export function login(data) {
-  return request({
-    url: '/vue-admin-template/user/login',
-    method: 'post',
-    data
-  })
-}
+export const login = data => request.post('', data)
 
-export function getInfo(token) {
-  return request({
-    url: '/vue-admin-template/user/info',
-    method: 'get',
-    params: { token }
-  })
-}
+export const getInfo = token => request.get('', token)
 
-export function logout() {
-  return request({
-    url: '/vue-admin-template/user/logout',
-    method: 'post'
-  })
-}
+export const logout = () => request.post('')
+
